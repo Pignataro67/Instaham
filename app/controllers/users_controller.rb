7a4989bce08dff4c491feb32eae4ed10e89fe818
Pignataro67 <Pignataro67@gmail.com>
2019-12-03 14:51:23 -0800

    more user controller crud, user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,4 +39,40 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params(:name, :password, :bio, :email, :number, :gender, :img_url))
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def login
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  def redirect
+    redirect_to session[:previous_url]
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params(*args)
+    params.require(:user).permit(*args)
+  end
 end
