3969ec21bb71048ec42119b6e8b363931c4e0452
Pignataro67 <Pignataro67@gmail.com>
2019-12-04 12:47:48 -0800

    adding db tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,2 @@
-require 'bcrypt'
 class User < ApplicationRecord
-  validates_confirmation_of :password
-  has_secure_password
-
-  def authenticate(plaintext_password)
-    BCrypt::Password.new(self.password_digest) == plaintext_password
-  end
 end
